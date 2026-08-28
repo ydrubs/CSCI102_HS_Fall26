421f44259df71cec731eaf87eef1dab87a18fc37
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-08-28 07:54:12 -0500

    initial commit

---
+++ Chapter 1/Exercises/Exercise 1.2 - Pizza Party.py
@@ -0,0 +1,43 @@
+"""
+Write a Python program that calculates information for a pizza party.
+
+Your program should do the following:
+
+    1. Ask the user to enter the number of guests, number of pizzas,
+       slices per pizza, and cost per pizza. Store each answer in a variable.
+
+    2. Convert the number of guests, number of pizzas, and slices per pizza
+       into integers. Convert the cost per pizza into a float.
+
+    3. Calculate the total number of slices amoung all the pizzas.
+
+    4. Calculate how many slices each guest can receive using quotient division.
+
+       Hint: Divide the total number of slices from all pizzas by the number
+       of guests. The result should NOT have a decimal.
+
+    5. Calculate how many slices will be left over after all slices are evenly divided using modulus.
+
+    6. Calculate the total cost of the pizzas.
+
+    7. Print a pizza party summary that clearly displays all calculated results.
+
+Example:
+    INPUT:
+        Enter the number of guests: 8
+        Enter the number of pizzas: 3
+        Enter the slices per pizza: 8
+        Enter the cost per pizza: 12.50
+
+    OUTPUT:
+        Pizza Party Calculator
+
+        Total slices: 24
+        Each guest can get 3 slices.
+        Slices left over: 0
+        Total pizza cost: $37.50
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
+#YOUR CODE HERE
