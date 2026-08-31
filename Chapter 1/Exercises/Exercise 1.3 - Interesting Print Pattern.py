d961108f5ff654ed481e40cd7442823bc1a4cd4f
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-08-31 08:49:57 -0500

    initial commit

---
+++ Chapter 1/Exercises/Exercise 1.3 - Interesting Print Pattern.py
@@ -0,0 +1,26 @@
+"""
+Exercise 1.3 - Interesting print pattern
+
+Use string Multiplication to create your own interesting pattern that is printed.
+
+   1) You should incorporate string multiplication (see slide 22 and 23) into your pattern
+   2) You may use as many lines of print as you want (3 to 10 is fair)
+   3) Don't use loops or other things we have not learned yet
+   4) DO NOT use AI to generate it, the goal is to think through the process
+
+
+   Example - Copy/paste the code below see what this looks like.
+        Make yours adequately different, don't just use different characters or numbers.
+
+            print(" " * 8 + "/\\")
+            print(" " * 6 + "/" + "=" * 4 + "\\")
+            print(" " * 4 + "/" + "o" * 2 + "-" * 2 + "o" * 2 + "\\")
+            print(" " * 2 + "/" + "~" * 10 + "\\")
+            print(" " + "|" + "#" * 4 + "@" * 4 + "#" * 4 + "|")
+            print(" " + "|" + "+" * 3 + "HELLO" + "+" * 3 + "|")
+            print(" " + "|" + "%" * 12 + "|")
+            print(" " * 2 + "\\" + "_" * 10 + "/")
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
