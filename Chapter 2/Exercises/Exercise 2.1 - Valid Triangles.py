1439ade5f04025aebb8ad38f4467486008c5c904
Yuriy Drubinskiy <ydrubins@yahoo.com>
2026-09-18 08:53:14 -0500

    initial commit

--- Chapter 2/Exercises/Exercise 2.1 - Valid Triangles.py
+++ Chapter 2/Exercises/Exercise 2.1 - Valid Triangles.py
@@ -9,6 +9,32 @@ Exercise 3.1 - Valid Triangles
     Remember that it should validate the triangle regardless of which side entered is the longest
     or if two of the three are equal.
 
+    Examples:
+
+        Test 1:
+            Enter the length of side 1: 3.0
+            Enter the length of side 2: 4.0
+            Enter the length of side 3: 5.0
+            Valid Triangle
+
+        Test 2:
+            Enter the length of side 1: 2.5
+            Enter the length of side 2: 2.5
+            Enter the length of side 3: 4.0
+            Valid Triangle
+
+        Test 3:
+            Enter the length of side 1: 3.0
+            Enter the length of side 2: 7.0
+            Enter the length of side 3: 2.0
+            Invalid Triangle
+
+        Test 4:
+            Enter the length of side 1: 10.0
+            Enter the length of side 2: 4.0
+            Enter the length of side 3: 5.0
+            Invalid Triangle
+
 
     :Author: <NAME>
     :Date: <TODAY'S DATE>
